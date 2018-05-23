a6aaec83f0c7f7d71ed24e9c8ccb12ce57b4319f
Kew Forest Teacher <daronson@kewforest.org>
2018-05-23 13:41:57 -0400

    added comments

--- Aero-pygame.py
+++ Aero-pygame.py
@@ -1,27 +1,35 @@
+#pygame is a module 
 import pygame
 
+#intitialize pygame - turn it on
 pygame.init()
+
 
 display_width = 1080
 display_height = 720
 
+#make the window
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Too Racey')
 
+#make some colors (R,G,B)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+
 clock = pygame.time.Clock()
 crashed = False
+
+#load image
 dronImg = pygame.image.load('drone.jpg')
 
-
+#function to draw the image
 def dron(x, y):
     gameDisplay.blit(dronImg, (x, y))
 
 
-x = (display_width * 0.45)
-y = (display_height * 0.1)
+x = (display_width * 0.45) #set x to the middle 
+y = (display_height * 0.1)#set y to bottom
 
 while not crashed:
     for event in pygame.event.get():
